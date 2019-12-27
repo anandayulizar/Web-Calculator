c7b796d01d822d51a010c0fc1491c5bb7d0dfb7e
yulizar57 <anandayulizar@gmail.com>
2019-12-27 21:35:26 +0700

    show result in calculator (not console anymore)

--- simpleCalculator.js
+++ simpleCalculator.js
@@ -1,4 +1,5 @@
-let result = document.getElementById('result');
+let result = document.querySelector('#result span:nth-child(2)');
+let temp = document.querySelector('#result span:nth-child(1)');
 let op = document.getElementsByClassName('op');
 let num = document.getElementsByClassName('num');
 let special = document.getElementsByClassName('special');
@@ -9,7 +10,6 @@ let specialAction;
 let showResult = false;
 
 function doOperation() {
-    if(activeOp != 0) {
         console.log('value 1 is ' + tempValue1);
         console.log('value 2 is ' + tempValue2);
         
@@ -30,9 +30,7 @@ function doOperation() {
             tempValue2 = 0;
         }
 
-        console.log('Result is ' + tempValue1);
-    }
-    
+        result.innerHTML = tempValue1;
 }
 
 function doSpecial() {
@@ -40,17 +38,21 @@ function doSpecial() {
         tempValue1 = 0;
         tempValue2 = 0;
         activeOp = 0;
+        result.innerHTML = 0;
+        temp.innerHTML = '';
     } else if (specialAction == '=') {
         doOperation();
         activeOp = 0;
         showResult = true;
+        temp.innerHTML = '';
     } else if (specialAction == 'Del') {
         if (activeOp == 0 && showResult == false) {
             tempValue1 = Math.floor(tempValue1 / 10);
-            console.log('value 1 now is ' + tempValue1);
+            result.innerHTML = tempValue1;
         } else {
             tempValue2 = Math.floor(tempValue2 / 10);
-            console.log('value 2 now is ' + tempValue2);
+            temp.innerHTML = ''+tempValue1+' '+activeOp+'';
+            result.innerHTML = tempValue2;
         }
     }
 }
@@ -63,8 +65,10 @@ for (let i = 0; i < num.length; i++) {
         }
         if (activeOp == 0) {
             tempValue1 = (tempValue1 * 10) + parseInt(num[i].textContent);
+            result.innerHTML = ''+tempValue1+'';
         } else {
             tempValue2 = (tempValue2 * 10) + parseInt(num[i].textContent);
+            result.innerHTML = ''+tempValue2+'';
         }
     })
 }
@@ -72,9 +76,13 @@ for (let i = 0; i < num.length; i++) {
 for (let i = 0; i < op.length; i++) {
     op[i].addEventListener('click', function() {
         showResult = false;
-        console.log('You pressed' + op[i].textContent);
-        doOperation();
+        if (activeOp != 0) {
+            doOperation();
+        }
+        
         activeOp = op[i].textContent;
+        temp.innerHTML = ''+tempValue1+' '+activeOp+'';
+        result.innerHTML = '0';
     }); 
 }
 
